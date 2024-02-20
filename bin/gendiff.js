f19b8852c79334e9ac4e16bad4c646324928f21d
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-02-20 23:55:50 +0300

    add arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,20 +1,22 @@
 #!/usr/bin/env node
 import { program } from 'commander';
 
-if (program.help) {
-  console.log(`
-    Usage: gendiff [options]
-
-    Compares two configuration files and shows a difference.
-
-    Options:
-      -V, --version        output the version number
-      -h, --help           output usage information
-  `);
-}
-
 program
   .description('Compares two configuration files and shows a difference.')
   .option('-v, --version', 'output the version number')
-  .option('-h, --help', 'output usage information')
-  .parse(process.argv);
+  .option('--format [type]', 'output format')
+  .parse(process.argv)
+  .arguments('<filepath1> <filepath2>');
+
+program.on('-h', () => {
+  console.log(`
+      Usage: gendiff [options] <filepath1> <filepath2>
+    
+      Compares two configuration files and shows a difference.
+    
+      Options:
+        -v, --version        output the version number
+        -f, --format [type]  output format
+        -h, --help           output usage information
+    `);
+});

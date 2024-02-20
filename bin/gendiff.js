40b9dd3179a958156daeb6caedbc6aa2fb0081fa
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-02-20 23:14:28 +0300

    creating help output

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,2 +1,20 @@
 #!/usr/bin/env node
-console.log('hello');
+import { program } from 'commander';
+
+if (program.help) {
+  console.log(`
+    Usage: gendiff [options]
+
+    Compares two configuration files and shows a difference.
+
+    Options:
+      -V, --version        output the version number
+      -h, --help           output usage information
+  `);
+}
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .option('-v, --version', 'output the version number')
+  .option('-h, --help', 'output usage information')
+  .parse(process.argv);

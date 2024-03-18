14027e26e5476c7c9e96b934aeca30db24958b92
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-03-18 22:40:47 +0300

    installing jest and setting up the linter, fixing the code

--- src/index.js
+++ src/index.js
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
-import readAndParseFile from "./util/parse.js";
+import readAndParseFile from './util/parse.js';
 
 const genDiff = (filePath1, filePath2) => {
-    const data1 = readAndParseFile(filePath1);
-    const data2 = readAndParseFile(filePath2);
+  const data1 = readAndParseFile(filePath1);
+  const data2 = readAndParseFile(filePath2);
 
-    const keys = Array.from(new Set([...Object.keys(data1),...Object.keys(data2)])).sort();
-    const diff = keys.map((key) => {
-        if (!data1.hasOwnProperty(key)) {
-            return `+ ${key}: ${data2[key]}`;
-        }
-        if(!data2.hasOwnProperty(key)) {
-            return `- ${key}: ${data1[key]}`;
-        }
-        if(data1[key] === data2[key]) {
-            return `  ${key}: ${data1[key]}`;
-        }
+  const keys = Array.from(new Set([...Object.keys(data1), ...Object.keys(data2)])).sort();
+  const diff = keys.map((key) => {
+    if (!Object.prototype.hasOwnProperty.call(data1, key)) {
+      return `+ ${key}: ${data2[key]}`;
+    }
+    if (!Object.prototype.hasOwnProperty.call(data2, key)) {
+      return `- ${key}: ${data1[key]}`;
+    }
+    if (data1[key] === data2[key]) {
+      return `  ${key}: ${data1[key]}`;
+    }
 
-        return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
-    });
+    return `- ${key}: ${data1[key]}\n+ ${key}: ${data2[key]}`;
+  });
 
-    return `{\n${diff.join('\n')}\n}`;
+  return `{\n${diff.join('\n')}\n}`;
 };
 
 export default genDiff;
--- src/util/parse.js
+++ src/util/parse.js
@@ -1,9 +1,8 @@
 import fs from 'fs';
-import { baseURL } from '../config.js';
 
 const readAndParseFile = (filePath) => {
-    const data = fs.readFileSync(filePath, 'utf-8');
-    return JSON.parse(data);
+  const data = fs.readFileSync(filePath, 'utf-8');
+  return JSON.parse(data);
 };
 
 export default readAndParseFile;

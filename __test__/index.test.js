d7871c6b8827bddc63e377850b4e1e1fd5319e4d
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-03-20 00:42:19 +0300

    writing tests

---
+++ __test__/index.test.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-undef */
+import path from 'path';
+import { fileURLToPath } from 'url';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (fileName) => path.join(`${__dirname}/__fixtures__`, fileName);
+
+test('should generate sorted difference between two objects', () => {
+  const filePath1 = getFixturePath('dataAny1.json');
+  const filePath2 = getFixturePath('dataAny2.json');
+
+  const expectedDiff = `{
+- follow: false
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: true
+}`;
+
+  const result = genDiff(filePath1, filePath2);
+  expect(result).toEqual(expectedDiff);
+});
+
+test('should return a sorted object for the same data', () => {
+  const filePath1 = getFixturePath('dataAny1.json');
+  const filePath2 = getFixturePath('dataEqual.json');
+
+  const expectedDiff = `{
+  follow: false
+  host: hexlet.io
+  proxy: 123.234.53.22
+  timeout: 50
+}`;
+
+  const result = genDiff(filePath1, filePath2);
+  expect(result).toEqual(expectedDiff);
+});

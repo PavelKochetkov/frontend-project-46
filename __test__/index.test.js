2126f62a5aff0d6ebe0e3e3f32887831bac9f3f3
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-03-26 22:50:38 +0300

    bug fix in getFixturePath function

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -2,9 +2,6 @@
 
 import genDiff from '../src/index.js';
 import { getFixturePath } from '../src/util/utilites.js';
-// import getPath from '../src/util/utilites.js';
-
-// const path = './__test__/__fixtures__';
 
 test('should generate sorted difference between two objects', () => {
   const filePath1 = getFixturePath('dataAny1.json', '__test__/__fixtures__');
@@ -23,25 +20,25 @@ test('should generate sorted difference between two objects', () => {
   expect(result).toEqual(expectedDiff);
 });
 
-// test('should return a sorted object for the same data', () => {
-//   const filePath1 = getPath(path, 'dataAny1.json');
-//   const filePath2 = getPath(path, 'dataEqual.json');
+test('should return a sorted object for the same data', () => {
+  const filePath1 = getFixturePath('dataAny1.json', '__test__/__fixtures__');
+  const filePath2 = getFixturePath('dataEqual.json', '__test__/__fixtures__');
 
-//   const expectedDiff = `{
-//   follow: false
-//   host: hexlet.io
-//   proxy: 123.234.53.22
-//   timeout: 50
-// }`;
+  const expectedDiff = `{
+  follow: false
+  host: hexlet.io
+  proxy: 123.234.53.22
+  timeout: 50
+}`;
 
-//   const result = genDiff(filePath1, filePath2);
-//   expect(result).toEqual(expectedDiff);
-// });
+  const result = genDiff(filePath1, filePath2);
+  expect(result).toEqual(expectedDiff);
+});
 
-// test('should return a string', () => {
-//   const filePath1 = getPath(path, 'dataAny1.json');
-//   const filePath2 = getPath(path, 'dataAny1.json');
+test('should return a string', () => {
+  const filePath1 = getFixturePath('dataAny1.json', '__test__/__fixtures__');
+  const filePath2 = getFixturePath('dataAny1.json', '__test__/__fixtures__');
 
-//   const result = genDiff(filePath1, filePath2);
-//   expect(typeof result).toBe('string');
-// });
+  const result = genDiff(filePath1, filePath2);
+  expect(typeof result).toBe('string');
+});

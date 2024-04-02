960feb151df47385fb07420f62196c4a2da57797
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-04-02 22:56:46 +0300

    changing file directory, fixing errors

--- src/index.js
+++ src/index.js
@@ -6,28 +6,33 @@ import { getFixturePath } from './util/utilites.js';
 const buildTree = (data1, data2) => {
   const keys = Array.from(new Set([...Object.keys(data1), ...Object.keys(data2)])).sort();
   const tree = keys.map((key) => {
-    if (_.has(data1, key)) {
-      return { key, value: data2[key] };
+    if (!_.has(data1, key)) {
+      return { key, type: 'added', value: data2[key] };
     }
     if (!_.has(data2, key)) {
-      return { key, value: data1[key] };
+      return { key, type: 'removed', value: data1[key] };
     }
     if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-      return { key, children: buildTree(data1[key], data2[key]) };
+      return { key, type: 'nested', children: buildTree(data1[key], data2[key]) };
     }
     if (_.isEqual(data1[key], data2[key])) {
-      return { key, value: data1[key] };
+      return { key, type: 'unchanged', value: data1[key] };
     }
 
-    return { key, oldValue: data1[key], newValue: data2[key] };
+    return {
+      key,
+      type: 'changed',
+      value1: data1[key],
+      value2: data2[key],
+    };
   });
 
   return tree;
 };
 
 const genDiff = (fileName1, fileName2) => {
-  const path1 = getFixturePath(fileName1, 'public');
-  const path2 = getFixturePath(fileName2, 'public');
+  const path1 = getFixturePath(fileName1, '__test__/__fixtures__');
+  const path2 = getFixturePath(fileName2, '__test__/__fixtures__');
   const data1 = readAndParseFile(path1);
   const data2 = readAndParseFile(path2);
 
--- src/util/utilites.js
+++ src/util/utilites.js
@@ -4,6 +4,6 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const getFixturePath = (filename, folder) => path.resolve(__dirname, '../..', `${folder}`, filename);
+const getFixturePath = (filename, folder) => path.resolve(__dirname, '../..', folder, filename);
 
 export { getFixturePath };

6e011e536049b2df2914bdc39a73649873311c5b
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-04-13 23:45:31 +0300

    Implementing a formatter, writing tests

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -1,17 +1,12 @@
 /* eslint-disable no-case-declarations */
-const getIdent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount - 2);
+const getIdent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount);
 
-const stringify = (value, replacer, space) => {
-  if (typeof value === 'string') {
-    return `${value}`;
-  }
+const stringify = (value, space) => {
   if (typeof value === 'object' && value !== null) {
-    return `{
-      ${Object.entries(value).map(([key, val]) => `${getIdent(space - 1)}${key}: ${stringify(val, null, space + 1)}`).join('\n')}
-    }`;
+    return `{\n${Object.entries(value).map(([key, val]) => `${getIdent(space)}${key}: ${stringify(val, space + 1)}`).join('\n')}\n${getIdent(space - 1)}}`;
   }
 
-  return JSON.stringify(value, replacer, space);
+  return `${value}`;
 };
 
 const formatStylish = (tree) => {
@@ -19,25 +14,23 @@ const formatStylish = (tree) => {
     const result = node.map((item) => {
       switch (item.type) {
         case 'added':
-          return `${getIdent(depth)}+ ${item.key}: ${stringify(item.value, null, depth + 1)}`;
+          return `${getIdent(depth)}+ ${item.key}: ${stringify(item.value, depth + 1)}`;
         case 'removed':
-          return `${getIdent(depth)}- ${item.key}: ${stringify(item.value, null, depth + 1)}`;
+          return `${getIdent(depth)}- ${item.key}: ${stringify(item.value, depth + 1)}`;
         case 'changed':
-          const add = `${getIdent(depth)}+ ${item.key}: ${stringify(item.value2, null, depth + 1)}`;
-          const del = `${getIdent(depth)}- ${item.key}: ${stringify(item.value1, null, depth + 1)}`;
+          const add = `${getIdent(depth)}+ ${item.key}: ${stringify(item.value2, depth + 1)}`;
+          const del = `${getIdent(depth)}- ${item.key}: ${stringify(item.value1, depth + 1)}`;
 
           return `${del}\n${add}`;
         case 'unchanged':
-          return `${getIdent(depth)}  ${item.key}: ${stringify(item.value, null, depth + 1)}`;
+          return `${getIdent(depth)}  ${item.key}: ${stringify(item.value, depth + 1)}`;
         case 'nested':
           return `${getIdent(depth)}  ${item.key}: ${iter(item.children, depth + 1)}`;
         default:
           throw new Error(`Unknown type: ${item.type}`);
       }
     });
-    return `{
-      \n${result.join('\n')}\n
-    }`;
+    return `{\n${result.join('\n')}\n${getIdent(depth - 1)}}`;
   };
   return iter(tree, 1);
 };

4688653d930573643590d0ee2387643acb9f52af
Pavel Kochetkov <kochetkov_p@mail.ru>
2024-04-23 23:16:00 +0300

    fix space count

--- src/formaters/stylish.js
+++ src/formaters/stylish.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-case-declarations */
-const getIdent = (depth, spaceCount = 4) => ' '.repeat(depth * spaceCount);
+const getIdent = (spaceCount) => ' '.repeat(spaceCount * 2);
 
 const stringify = (value, space) => {
   if (typeof value === 'object' && value !== null) {
@@ -14,16 +14,16 @@ const formatStylish = (tree) => {
     const result = node.map((item) => {
       switch (item.type) {
         case 'added':
-          return `${getIdent(depth)}+ ${item.key}: ${stringify(item.value, depth + 1)}`;
+          return `${getIdent(depth + 1)}+ ${item.key}: ${stringify(item.value, depth + 3)}`;
         case 'removed':
-          return `${getIdent(depth)}- ${item.key}: ${stringify(item.value, depth + 1)}`;
+          return `${getIdent(depth + 1)}- ${item.key}: ${stringify(item.value, depth + 3)}`;
         case 'changed':
-          const add = `${getIdent(depth)}+ ${item.key}: ${stringify(item.value2, depth + 1)}`;
-          const del = `${getIdent(depth)}- ${item.key}: ${stringify(item.value1, depth + 1)}`;
+          const add = `${getIdent(depth + 1)}+ ${item.key}: ${stringify(item.value2, depth + 3)}`;
+          const del = `${getIdent(depth + 1)}- ${item.key}: ${stringify(item.value1, depth + 3)}`;
 
           return `${del}\n${add}`;
         case 'unchanged':
-          return `${getIdent(depth)}  ${item.key}: ${stringify(item.value, depth + 1)}`;
+          return `${getIdent(depth + 1)}  ${item.key}: ${stringify(item.value, depth + 3)}`;
         case 'nested':
           return `${getIdent(depth)}  ${item.key}: ${iter(item.children, depth + 1)}`;
         default:
